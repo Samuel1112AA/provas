5fa331b11b505dd4463b7b388e7451c65a94baa3
isaaaccc <148624583+Samuel1112AA@users.noreply.github.com>
2023-12-09 07:44:54 -0300

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,8 @@
+velocidade = int(input("qual a sua velocidade: "))
+if velocidade > 80:
+    print("voce foi multado, seu roda dura")
+elif velocidade < 80:
+    print("voce passou no teste do detran")
+elif velocidade == 80:
+    print("voce passou no teste do detran")
+
